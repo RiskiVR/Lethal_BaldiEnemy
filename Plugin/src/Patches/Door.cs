aafb4ed043ef709f9613df50149d83b28e967c36
Robyn <rnixon98@gmail.com>
2024-10-23 04:01:57 -0500

    fix: target correct class for door patches rip

--- Plugin/src/Patches/Door.cs
+++ Plugin/src/Patches/Door.cs
@@ -7,8 +7,8 @@ public class DoorPatch
 {
 
     [HarmonyPostfix]
-    [HarmonyPatch(typeof(Door), nameof(Door.SetDoorState))]
-    public static void DoorStatePatch(Door __instance)
+    [HarmonyPatch(typeof(DoorLock), nameof(DoorLock.OpenOrCloseDoor))]
+    public static void DoorStatePatch(DoorLock __instance)
     {
         Plugin.Logger.LogInfo($"A door has changed state at position {__instance.transform.position}");
         BaldiHearingManager.AlertBaldiToDoorSound(__instance.transform.position);
